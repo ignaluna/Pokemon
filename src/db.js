c1cc82c00935ae1ad99f1d5c993d07e9888ccb5a
Igna Luna <ignaluna98pb@gmail.com>
2023-02-22 14:40:20 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -15,7 +15,12 @@ const { DB_DEPLOY} = process.env
 
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  native: false,
+  dialectOptions:{
+    ssl: {
+      require: true,
+    },
+  } // lets Sequelize know we can use pg-native for ~30% more speed
 });
 
 const basename = path.basename(__filename);
